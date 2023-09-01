3dada143ecf042a03087710e7fd16fc6801691b1
Tiago Batista Corrêa <tiagobc@live.com>
2023-09-01 15:20:19 -0300

    CRUD de exemplo básico

---
+++ src/main/java/com/tiagocorrea/listasupermercado/ListasupermercadoApplication.java
@@ -0,0 +1,13 @@
+package com.tiagocorrea.listasupermercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListasupermercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListasupermercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiagocorrea/listasupermercado/ListasupermercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiagocorrea.listasupermercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListasupermercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
